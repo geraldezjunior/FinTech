f47e3cdb35acbde043ee8eb1400e1a975ff90247
Geraldez Junior <geraldezjunior09@gmail.com>
2026-02-01 22:56:31 -0300

    sidenav

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -4,10 +4,13 @@ import DateRange from "./DateRange";
 import Month from "./Month";
 
 const Header = () => {
+  const [title, setTitle] = React.useState("Resumo");
+
   return (
     <header className="mb">
-      <div className="mb">
+      <div className="daterange mb">
         <DateRange />
+        <h1 className="box bg-3">{title}</h1>
       </div>
       <Month />
     </header>
